58160519162d5a8bd1c7a1dd3a6cbf914b9e7bcc
adyeetya <2612adityasingh2000@gmail.com>
2025-05-05 22:20:08 +0530

    backend-ip

--- app/config.js
+++ app/config.js
@@ -1,2 +1,4 @@
-export const ServerUrl = 'http://localhost:5009/api/v1'
-// /tripPlans/getAllIds
+export const ServerUrl = 'https://backend.travelchapes.com/api/v1'
+// 'http://localhost:5009/api/v1'
+
+// 'http://13.49.44.64:5009/api/v1'; 'https://backend.travelchapes.com/api/v1'
